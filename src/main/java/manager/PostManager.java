89ab6ab3c7c84eec53dcd9f033469e48e5466873
alfiiasharipova <alfiiasharipova@gmail.com>
2020-11-27 15:13:44 +0300

    Post manager created

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,17 @@
+package manager;
+
+import domain.Post;
+
+import java.util.List;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] wallSearch(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset, boolean extended, List<String> fields){
+        //to do
+        return null;
+    }
+    public void wallDelete(int ownerId, int postId){
+        //to do
+    }
+}
